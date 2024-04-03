61604eb7f01d490401eb7f6f12b4e454deef7f30
markalvaress <mark.alvarez2@gmail.com>
2024-04-03 16:39:09 +0100

    Added some primitive scrolling

--- conways_game.py
+++ conways_game.py
@@ -57,41 +57,58 @@ def determine_outcome(grid, position_x, position_y):
 
 
 def conway(stdscr):
+    pad_height = 300
+    pad_width = 300
+    pad = curses.newpad(pad_height, pad_width)
+    pad.clear()
+    pad.keypad(True)
     k = 0
-    stdscr.clear()
-    stdscr.refresh()
-
-    height, width = stdscr.getmaxyx()
-
+    window_loc_y = int(pad_height/2)
+    window_loc_x = int(pad_width/2) # where the window starts within the pad
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+    height = curses.LINES
+    width = curses.COLS
+ 
+    grid = np.zeros((pad_height - 1, pad_width - 1), dtype = bool) # presumably dtype bool will be faster than using int 0 and 1
 
-    grid = np.zeros((height - 1, width - 1), dtype = bool) # presumably dtype bool will be faster than using int 0 and 1
-
+    # Generate some random alive cells to fill in the frid
     rng = np.random.default_rng()
-    rand_rownums = rng.integers(height - 2, size = int(width*height / 2))
-    rand_colnums = rng.integers(width - 2, size = int(width*height / 2))
-    rand_points = zip(rand_rownums, rand_colnums) # there's a change there's duplicate points here but I'm not that bothered at this stage
+    rand_rownums = rng.integers(pad_height - 2, size = int(pad_width*pad_height / 2))
+    rand_colnums = rng.integers(pad_width - 2, size = int(pad_width*pad_height / 2))
+    rand_points = zip(rand_rownums, rand_colnums) # there's duplicate points here but I'm not that bothered at this stage
 
     for point in rand_points:
         grid[point[0], point[1]] = True
 
     while (k != ord('q')):
-        stdscr.clear()
-        stdscr.attron(curses.color_pair(1))
-        new_grid = np.zeros((height - 1, width - 1), dtype = bool)
+        pad.clear()
+        pad.attron(curses.color_pair(1))
+        new_grid = np.zeros((pad_height - 1, pad_width - 1), dtype = bool)
+
+        # Move screen when arrow keys pressed
+        if k == curses.KEY_UP:
+            window_loc_y -= 1
+        elif k == curses.KEY_RIGHT:
+            window_loc_x += 1
+        elif k == curses.KEY_DOWN:
+            window_loc_y += 1
+        elif k == curses.KEY_LEFT:
+            window_loc_x -= 1
 
         # Draw current grid and work out state in next time period
-        for x in range(width - 1):
-            for y in range(height - 1):
+        for x in range(pad_width - 1):
+            for y in range(pad_height - 1):
                 if grid[y,x]:
-                    stdscr.addch(y, x, " ")
+                    pad.addch(y, x, " ")
                 new_grid[y,x] = determine_outcome(grid, x, y)
                 
-        # first draw current grid, then update with new grid data and wait
-        stdscr.refresh()
+        # first display current grid, then update with new grid data and wait
+        pad.refresh(window_loc_y, window_loc_x, 
+                    0,0 ,
+                    height - 1, width - 1)
         grid = new_grid
 
-        k = stdscr.getch()
+        k = pad.getch()
 
 def main():
     curses.wrapper(conway)
